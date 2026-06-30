77f565fdb2f58bb8141a840c995e8fc19b081056
babossene <senebabos1986@gmail.com>
2026-06-30 16:59:39 +0000

    partie3

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #1
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
-from models import (supplier_pydantic, supplier_pydanticIn, Supplier)
+from models import (supplier_pydantic, supplier_pydanticIn, Supplier, 
+product_pydantic, product_pydanticIn, Product) 
 
 app = FastAPI()
 
@@ -41,6 +42,44 @@ async def update_supplier(supplier_id: int, update_info: supplier_pydanticIn):
 @app.delete('/supplier/{supplier_id}')
 async def delete_supplier(supplier_id: int):
     await Supplier.get(id=supplier_id).delete()
+    return {"status": "ok"}
+
+
+@app.post('/product/{supplier_id}')
+async def  add_product(supplier_id: int, products_details: product_pydanticIn):
+    supplier = await Supplier.get(id = supplier_id)
+    products_details = products_details.dict(exclude_unset = True)
+    products_details['revenue'] += products_details['quantity_sold'] * products_details['unit_price']
+    product_obj = await Product.create(**products_details, supplied_by = supplier)
+    response = await product_pydantic.from_tortoise_orm(product_obj)
+    return {"status": "ok", "data" : response}
+
+@app.get ('/product')
+async def all_products():
+    response = await product_pydantic.from_queryset_single(Product.all())
+    return {"status": "ok", "data" : response}
+
+@app.get('/product/id{}')
+async def specific_product(id=int):
+    response = await product_pydantic.from_queryset_single(Product.get(id=id))
+    return {"status": "ok", "data" : response}
+
+@app.put('/product/{id}')
+async def update_product(id: int, update_info: product_pydantic):
+    product = Product.get(id = id)
+    update_info = update_info.dict(exclude_unset = True)
+    product.name = update_info['name']
+    product.quantity_in_stock = update_info['quantity_in_stock']
+    product.revenue += update_info['quantity_sold'] * update_info['unit_price']
+    product.quantity_sold +=update_info['quantity_sold']
+    product.unit_price = update_info['unit_price']
+    await product.save()
+    response = await product_pydantic.from_tortoise_orm(product)
+    return {"status": "ok", "data" : response}
+
+@app.delete('/product/id')
+async def delete_product(id:int):
+    await Product.filter(id=id).delete()
     return {"status": "ok"}
 
 
