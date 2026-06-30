7455cf7738ddd76666fcc9122a6b7e11c66da09d
babossene <senebabos1986@gmail.com>
2026-06-30 13:38:10 +0000

    partie2

--- app.py
+++ app.py
@@ -1,14 +1,50 @@
 #1
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
+from models import (supplier_pydantic, supplier_pydanticIn, Supplier)
 
 app = FastAPI()
 
 @app.get('/')
 def index():
-    return{"Msg" : "Hello world"}
+    return{"Msg" : "go to the API documentation"}
 
-#3 registre tortoisr
+#4
+@app.post('/supplier')
+async def add_supplier(supplier_info: supplier_pydanticIn):
+    supplier_obj = await Supplier.create(**supplier_info.dict(exclude_unset=True))
+    response = await supplier_pydantic.from_tortoise_orm(supplier_obj)
+    return {"status": "ok", "data" : response}
+
+@app.get('/supplier')
+async def get_all_supplier():
+    response = await supplier_pydantic.from_queryset(Supplier.all())
+    return {"status": "ok", "data" : response}
+
+@app.get('/supplier/{suppliers_id}')
+async def get_specific_supplier(supplier_id: int):
+    response = await supplier_pydantic.from_queryset_single(Supplier.get(id=supplier_id))
+    return {"status": "ok", "data" : response}
+
+@app.put('/supplier/{supplier_id}')
+async def update_supplier(supplier_id: int, update_info: supplier_pydanticIn):
+    supplier = await Supplier.get(id=supplier_id)
+    update_info = update_info.dict(exclude_unset=True)
+    supplier.name = update_info['name']
+    supplier.company = update_info['company']
+    supplier.phone = update_info['phone']
+    supplier.email = update_info['email']
+    await supplier.save()
+    response = await supplier_pydantic.from_tortoise_orm(supplier)
+    return {"status": "ok", "data" : response}
+
+@app.delete('/supplier/{supplier_id}')
+async def delete_supplier(supplier_id: int):
+    await Supplier.get(id=supplier_id).delete()
+    return {"status": "ok"}
+
+
+#3 registre tortoise
 
 register_tortoise(
     app,
@@ -17,4 +53,6 @@ register_tortoise(
     generate_schemas=True,
     add_exception_handlers=True
 )
+
+
 
